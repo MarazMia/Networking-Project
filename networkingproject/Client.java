37a3f8df3d6c888cbee7028cc4ae3de700e2818f
MARAZ MIA <maraj262000@gmail.com>
2021-06-10 12:46:12 +0600

    client side code full complete

--- networkingproject/Client.java
+++ networkingproject/Client.java
@@ -9,8 +9,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -25,6 +26,8 @@ public class Client {
     public static void main(String[] args) {
 
         final File[] fileToSend = new File[1];
+        final String hostNo;
+        final int portNo;
 
         //our project window
         JFrame window = new JFrame("Networking Project");
@@ -37,6 +40,34 @@ public class Client {
         intro.setFont(new Font("Arial", Font.BOLD, 25));
         intro.setBorder(new EmptyBorder(20, 0, 10, 0));
         intro.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        //host and port option
+        JPanel option = new JPanel();
+        option.setBorder(new EmptyBorder(50, 0, 0, 0));
+        option.setFont(new Font("Arial", Font.BOLD, 20));
+
+        JLabel hl = new JLabel("host   ");
+        JTextField hostOption = new JTextField("localhost", 10);
+        hostNo = hostOption.getText().toString().trim();
+        JLabel pl = new JLabel("port   ");
+        JTextField portOption = new JTextField("1234", 10);
+        String prtn = portOption.getText().toString().trim();
+        portNo = Integer.parseInt(prtn);
+        JButton connect = new JButton("connetct");
+
+        option.add(hl);
+        option.add(hostOption);
+        option.add(pl);
+        option.add(portOption);
+        option.add(connect);
+
+        JPanel conPane = new JPanel();
+        conPane.setBorder(new EmptyBorder(50, 0, 0, 0));
+        conPane.setFont(new Font("Arial", Font.BOLD, 20));
+        JLabel con = new JLabel("connection status");
+        JTextField conState = new JTextField("not connected", 10);
+        conPane.add(con);
+        conPane.add(conState);
 
         //file name label
         JLabel fileName = new JLabel("choose a file to send...");
@@ -51,13 +82,39 @@ public class Client {
         JButton sendFile = new JButton("send file");
         sendFile.setPreferredSize(new Dimension(150, 75));
         sendFile.setFont(new Font("Arial", Font.BOLD, 20));
+        sendFile.setEnabled(false);
 
         JButton chooseFile = new JButton("choose file");
         chooseFile.setPreferredSize(new Dimension(150, 75));
         chooseFile.setFont(new Font("Arial", Font.BOLD, 20));
+        chooseFile.setEnabled(false);
 
         jButtons.add(sendFile);
         jButtons.add(chooseFile);
+        
+       
+
+        connect.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (hostNo.isEmpty() || Integer.toString(portNo).isEmpty()) {
+                    intro.setText("please select host and port address");
+                } else {
+                    try {
+                        Socket socket = new Socket(hostNo, portNo);
+                        if(socket.isConnected()){
+                            conState.setText("connected on port "+portNo);
+                            sendFile.setEnabled(true);
+                            chooseFile.setEnabled(true);
+                        }
+                    } catch (IOException ex) {
+                        conState.setText("error occured");
+                        Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+
+        });
 
         chooseFile.addActionListener(new ActionListener() {
             @Override
@@ -80,19 +137,24 @@ public class Client {
                     fileName.setText("please choose a file first!!!");
                 } else {
                     try {
-                        FileInputStream fis = new FileInputStream(fileToSend[0].getAbsolutePath());  
-                        Socket socket = new Socket("localhost",1011);
-                        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                        
-                        String FileName = fileToSend[0].getName();
-                        byte[] FileNameBytes = FileName.getBytes();
-                        byte[] FileBytes = new byte[(int)fileToSend[0].length()];
-                        
-                        fis.read(FileBytes);
-                        dos.writeInt(FileNameBytes.length);
-                        dos.write(FileNameBytes);
-                        dos.write(FileBytes);
+                        FileInputStream fis = new FileInputStream(fileToSend[0].getAbsolutePath());
+                        if (hostNo.isEmpty() || Integer.toString(portNo).isEmpty()) {
+                            fileName.setText("please select host and port address");
+                        } else {
+                            Socket socket = new Socket(hostNo, portNo);
+                            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+                            String FileName = fileToSend[0].getName();
+                            byte[] FileNameBytes = FileName.getBytes();
+                            byte[] FileBytes = new byte[(int) fileToSend[0].length()];
+
+                            fis.read(FileBytes);
+                            dos.writeInt(FileNameBytes.length);
+                            dos.write(FileNameBytes);
+                            dos.write(FileBytes);
+                        }
                     } catch (IOException error) {
+                        fileName.setText("error occured when sending the file " + fileToSend[0].getName());
                         error.printStackTrace();
                     }
                 }
@@ -101,6 +163,8 @@ public class Client {
         });
 
         window.add(intro);
+        window.add(option);
+        window.add(conPane);
         window.add(fileName);
         window.add(jButtons);
         window.setVisible(true);
