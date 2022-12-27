38907b903578aac1222a4646977f56e8f480d49f
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2022-12-27 19:45:29 +0600

    Add files via upload

---
+++ ServerSide_GUI/src/Server.java
@@ -0,0 +1,248 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Server {
+
+    // Array list to hold information about the files received.
+    static ArrayList<MyFile> myFiles = new ArrayList<>();
+    static ArrayList<MyFile> allFiles = new ArrayList<>();
+    static ServerSocket serverSocket;
+
+    static {
+        try {
+            serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static Socket socket;
+    static DataInputStream dis;
+    static FileOutputStream fos;
+    static OutputStream os;
+    static String message;
+    static boolean flag = false;
+
+    static String home = System.getProperty("user.home");
+    static String filePath = home + "/Downloads/server_files";
+//    static String filePath = "src/server_files";
+
+    /*Server() throws IOException{
+        //fileNames();
+        serverSocket = new ServerSocket(1234);
+        socket = serverSocket.accept();
+        dis = new DataInputStream(socket.getInputStream());
+        fos = new FileOutputStream("src/server_files/");
+        //sendFile();
+    }*/
+    public static void main(String[] args) throws IOException {
+
+        int fileId = 0;
+
+        JFrame jFrame = new JFrame("Server");
+
+        jFrame.setSize(400, 400);
+
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel jPanel = new JPanel();
+
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+
+        JScrollPane jScrollPane = new JScrollPane(jPanel);
+
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JLabel jlTitle = new JLabel("File Receiver");
+
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
+
+        jlTitle.setBorder(new EmptyBorder(20, 0, 10, 0));
+
+        jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        jFrame.add(jlTitle);
+        jFrame.add(jScrollPane);
+
+        jFrame.setVisible(true);
+
+        fileNames();
+        System.out.println(allFiles.size());
+
+//        serverSocket = new ServerSocket(1234);
+        socket = serverSocket.accept();
+
+
+
+        while (true) {
+
+            try {
+
+
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                //message has been catched
+                int operationLength = dataInputStream.readInt();
+                byte[] opbyte = new byte[operationLength];
+                dataInputStream.readFully(opbyte, 0, opbyte.length);
+                String opName = new String(opbyte);
+
+                if (opName.equals("upload")) {
+
+                    int fileNameLength = dataInputStream.readInt();
+
+                    if (fileNameLength > 0) {
+
+                        byte[] fileNameBytes = new byte[fileNameLength];
+
+                        dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+
+                        String fileName = new String(fileNameBytes);
+
+                        int fileContentLength = dataInputStream.readInt();
+
+                        if (fileContentLength > 0) {
+
+                            byte[] fileContentBytes = new byte[fileContentLength];
+
+                            dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
+
+                            JPanel jpFileRow = new JPanel();
+                            jpFileRow.setLayout(new BoxLayout(jpFileRow, BoxLayout.X_AXIS));
+
+                            JLabel jlFileName = new JLabel(fileName);
+                            jlFileName.setFont(new Font("Arial", Font.BOLD, 20));
+                            jlFileName.setBorder(new EmptyBorder(10, 0, 10, 0));
+                            if (getFileExtension(fileName).equalsIgnoreCase("txt")) {
+
+                                jpFileRow.setName((String.valueOf(fileId)));
+
+                                jpFileRow.add(jlFileName);
+                                jPanel.add(jpFileRow);
+                                jFrame.validate();
+                            } else {
+
+                                jpFileRow.setName((String.valueOf(fileId)));
+
+                                jpFileRow.add(jlFileName);
+                                jPanel.add(jpFileRow);
+
+                                jFrame.validate();
+                            }
+
+                            myFiles.add(new MyFile(fileId, fileName, fileContentBytes, getFileExtension(fileName)));
+                            allFiles.add(new MyFile(fileId, fileName, fileContentBytes, getFileExtension(fileName)));
+
+                            fileId++;
+
+                            File fileToSave = new File(filePath + "/" + fileName);
+                            try {
+
+                                FileOutputStream fileOutputStream = new FileOutputStream(fileToSave);
+
+                                fileOutputStream.write(fileContentBytes);
+
+                                fileOutputStream.close();
+
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                } else if (opName.equals("delete")) {
+                    int FileId = dataInputStream.readInt();
+                    System.out.println("received id " + FileId);
+
+                    for (int i = 0; i < allFiles.size(); i++) {
+                        if (FileId == allFiles.get(i).id) {
+                            File f = new File(filePath + "/" + allFiles.get(i).name);
+                            System.out.println(f.delete());
+                            System.out.println(allFiles.get(i).name);
+                            allFiles.remove(allFiles.get(i));
+                            System.out.println("after removal " + allFiles.size());
+
+
+                        }
+                    }
+                } else if (opName.equals("download")) {
+
+                    os = socket.getOutputStream();
+                    ObjectOutputStream oos = new ObjectOutputStream(os);
+                    oos.writeObject(allFiles);
+                    //oos.close();
+
+                }
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                socket.close();
+                SwingUtilities.updateComponentTreeUI(jFrame);
+                break;
+//                if (e.getMessage().equals("Socket is closed")) {
+//                    //break;
+//                    SwingUtilities.updateComponentTreeUI(jFrame);
+//                }
+//                socket.close();
+
+                //break;
+                //e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static String getFileExtension(String fileName) {
+
+        int i = fileName.lastIndexOf('.');
+
+        if (i > 0) {
+
+            return fileName.substring(i + 1);
+        } else {
+            return "No extension found.";
+        }
+    }
+
+    public static void fileNames() throws IOException {
+        int fileId = 0;
+        File dir = new File(filePath);
+        File[] listOfFiles = dir.listFiles();
+        for (File file : listOfFiles) {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
+                String fileName = file.getName();
+                byte[] fileContentBytes = new byte[(int) file.length()];
+                if ((int) file.length() > 0) {
+                    fileInputStream.read(fileContentBytes);
+                }
+
+                MyFile newFile = new MyFile(fileId, fileName, fileContentBytes, getFileExtension(fileName));
+                newFile.setData(fileContentBytes);
+                allFiles.add(newFile);
+                fileId++;
+
+                System.out.println(newFile.getId() + " " + newFile.getName() + " " + newFile.getData().length + " " + newFile.getFileExtension());
+                fileInputStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
